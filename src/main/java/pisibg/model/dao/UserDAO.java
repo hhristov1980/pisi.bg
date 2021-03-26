3b02408c45436976c819daa009cf387ba3485a6a
ValentinNenkov <valio.nenkov@gmail.com>
2021-03-26 16:12:49 +0200

    user/admin functionality update

--- src/main/java/pisibg/model/dao/UserDAO.java
+++ src/main/java/pisibg/model/dao/UserDAO.java
@@ -20,7 +20,7 @@ public class UserDAO extends AbstractDAO {
 
     public void deleteUser(int id) throws SQLException {
         String sql = "UPDATE users SET email='deleted', password='deleted', first_name='deleted', last_name='deleted'," +
-                " phone_number='deleted', address='deleted',deleted_at = ? WHERE id = ?;";
+                " phone_number='deleted',address='deleted' , is_subscribed='0',deleted_at = ? WHERE id = ?;";
         String select = "SELECT * FROM users";
         try (Connection connection = jdbcTemplate.getDataSource().getConnection();
              PreparedStatement statement = connection.prepareStatement(sql);
@@ -28,6 +28,14 @@ public class UserDAO extends AbstractDAO {
             statement.setTimestamp(1, new Timestamp(System.currentTimeMillis()));
             statement.setInt(2, id);
             statement.executeUpdate();
+        }
+    }
+
+    public int countAdmin() throws SQLException{
+        String sql = "SELECT * FROM users WHERE is_admin = 1";
+        try (Connection connection = jdbcTemplate.getDataSource().getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql);){
+            return statement.executeUpdate();
         }
     }
 
